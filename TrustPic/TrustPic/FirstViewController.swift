c5f8de06ab04b88bdb93f5fc8eca5a58674d06e2
Aakash verma <vermaaakash1234@gmail.com>
2019-04-15 14:54:20 +0530

    First commit, created the project

---
+++ TrustPic/TrustPic/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TrustPic
+//
+//  Created by Aakash verma on 15/04/19.
+//  Copyright © 2019 norsky. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TrustPic/TrustPic/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TrustPic
+//
+//  Created by Aakash verma on 15/04/19.
+//  Copyright © 2019 norsky. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
