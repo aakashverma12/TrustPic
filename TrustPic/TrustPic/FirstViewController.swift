2bda41d6edb1ef13df89520a2613d48b866f3cdc
Aakash verma <vermaaakash1234@gmail.com>
2019-04-20 01:40:17 +0530

    Home is ready now

--- TrustPic/TrustPic/FirstViewController.swift
+++ TrustPic/TrustPic/FirstViewController.swift
@@ -14,8 +14,20 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var blackShadow: UIView!
     @IBOutlet weak var trust: UILabel!
     @IBOutlet weak var Pic: UILabel!
-    @IBOutlet weak var firstSlider: UISlider!
-    @IBOutlet weak var progBack: UIView!
+    @IBOutlet var progs: [UIView]!
+    @IBOutlet var sliders: [ColorSlider]!
+    
+    @IBAction func slideHandler(_ sender: ColorSlider) {
+        // this function handles the slider color change
+        let inval = Int(sender.value);
+        if (inval < 3) {
+            sender.minimumTrackTintColor = UIColor(red:1.00, green:0.18, blue:0.33, alpha:1.0);
+        } else if (inval >= 3 && inval <= 6 ) {
+            sender.minimumTrackTintColor = UIColor(red:0.33, green:0.64, blue:0.97, alpha:1.0);
+        } else {
+            sender.minimumTrackTintColor = UIColor(red:0.29, green:0.91, blue:0.57, alpha:1.0);
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,18 +40,21 @@ class FirstViewController: UIViewController {
         blackShadow.layer.shadowRadius = 6.0;
         blackShadow.clipsToBounds = false;
         blackShadow.layer.cornerRadius = 16.0;
-        firstSlider.setThumbImage(UIImage(), for: .normal);
-        firstSlider.layer.shadowColor = UIColor.black.cgColor;
-        firstSlider.layer.shadowOffset = CGSize(width: 0, height: 3);
-        firstSlider.layer.shadowOpacity = 0.2;
-        firstSlider.layer.shadowRadius = 6.0;
-        firstSlider.clipsToBounds = false;
-        progBack.layer.borderWidth = 1.0;
-        let opacity:CGFloat = 0.1;
-        progBack.layer.borderColor =    UIColor.black.withAlphaComponent(opacity).cgColor;
-        progBack.layer.cornerRadius = 4.0;
-        progBack.layer.masksToBounds = true;
-        
+        for firstSlider in sliders {
+            firstSlider.setThumbImage(UIImage(), for: .normal);
+            firstSlider.layer.shadowColor = UIColor.black.cgColor;
+            firstSlider.layer.shadowOffset = CGSize(width: 0, height: 3);
+            firstSlider.layer.shadowOpacity = 0.2;
+            firstSlider.layer.shadowRadius = 6.0;
+            firstSlider.clipsToBounds = false;
+        }
+        for progBack in progs {
+            progBack.layer.borderWidth = 1.0;
+            let opacity:CGFloat = 0.1;
+            progBack.layer.borderColor =    UIColor.black.withAlphaComponent(opacity).cgColor;
+            progBack.layer.cornerRadius = 4.0;
+            progBack.layer.masksToBounds = true;
+        }
     }
 }
 
